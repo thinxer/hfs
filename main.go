6839bdaca1ebcacf52d8da75a0f34c04f96ac861
Jianfei Wang <me@thinxer.com>
2014-12-25 01:03:04 +0800

    simple uploader

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"io"
+	"log"
+	"os"
+	"path"
 
 	"net/http"
 )
@@ -14,5 +18,44 @@ func main() {
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/_/upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			mr, err := r.MultipartReader()
+			check(err)
+			for {
+				p, err := mr.NextPart()
+				if err != nil {
+					break
+				}
+				name := path.Base(p.FileName())
+				f, err := os.Create(name)
+				check(err)
+				_, err = io.Copy(f, p)
+				check(err)
+				f.Close()
+				p.Close()
+			}
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		} else {
+			w.Write([]byte(tmplUpload))
+		}
+	})
+	log.Println("Listing at", *flagListen)
 	panic(http.ListenAndServe(*flagListen, nil))
 }
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+const tmplUpload = `
+<!DOCTYPE html>
+<html>
+<body>
+<form enctype="multipart/form-data" method="POST">
+    <input type="file" name="upload">
+    <button type="submit">upload</button>
+</form>
+`
